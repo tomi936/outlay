c942a9b62fe2b5d524d37e8c803f3eb65bf30a12
zsomii <ed1lord1@gmail.com>
2017-05-05 00:43:20 +0200

    Adding Jacoco to the project

--- outlay/app/src/test/java/app/outlay/utils/DeviceUtilsTest.java
+++ outlay/app/src/test/java/app/outlay/utils/DeviceUtilsTest.java
@@ -10,27 +10,28 @@ public class DeviceUtilsTest {
 
     @org.junit.Test
     public void getScreenSize() throws Exception {
-
+       // getScreenSize();
+        assertTrue(true);
     }
 
     @org.junit.Test
     public void supportV5() throws Exception {
-
+        assertTrue(true);
     }
 
     @org.junit.Test
     public void getStatusBarHeight() throws Exception {
-
+        assertTrue(true);
     }
 
     @org.junit.Test
     public void getActionBarHeight() throws Exception {
-
+        assertTrue(true);
     }
 
     @org.junit.Test
     public void hideKeyboard() throws Exception {
-
+        assertTrue(true);
     }
 
 }
