37844abf0c8c3af17d34a11bce05da8ddd4d537e
lycantroph <lycan@sch.bme.hu>
2017-05-04 13:06:35 +0200

    Creating Unit test framework

---
+++ outlay/app/src/test/java/app/outlay/utils/DeviceUtilsTest.java
@@ -0,0 +1,34 @@
+package app.outlay.utils;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lycan on 2017. 05. 04..
+ */
+public class DeviceUtilsTest {
+    @org.junit.Test
+    public void getScreenSize() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void supportV5() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getStatusBarHeight() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getActionBarHeight() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void hideKeyboard() throws Exception {
+
+    }
+
+}
